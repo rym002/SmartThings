e3a6e8a053d9f4b228bb49e5df7019b7cf78e195
rym002 <raymond.munian@gmail.com>
2020-06-07 13:41:28 -0400

    Add functions

--- devicetypes/rym002/jasco-zwave-dimmer/jasco-zwave-dimmer.groovy
+++ devicetypes/rym002/jasco-zwave-dimmer/jasco-zwave-dimmer.groovy
@@ -60,3 +60,99 @@
 
 	}
 }
+def installed() {
+	// Device-Watch simply pings if no device events received for checkInterval duration of 32min = 2 * 15min + 2min lag time
+	sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
+}
+
+def updated() {
+	response(refresh())
+}
+
+def parse(description) {
+	def result = null
+	if (description.startsWith("Err 106")) {
+		result = createEvent(descriptionText: description, isStateChange: true)
+	} else if (description != "updated") {
+		def cmd = zwave.parse(description, [0x20: 1, 0x25: 1, 0x70: 1, 0x98: 1])
+		if (cmd) {
+			result = zwaveEvent(cmd)
+			log.debug("'$description' parsed to $result")
+		} else {
+			log.debug("Couldn't zwave.parse '$description'")
+		}
+	}
+	result
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd) {
+	createEvent(name: "switch", value: cmd.value ? "on" : "off")
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicSet cmd) {
+	createEvent(name: "switch", value: cmd.value ? "on" : "off")
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd) {
+	createEvent(name: "switch", value: cmd.value ? "on" : "off")
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.hailv1.Hail cmd) {
+	createEvent(name: "hail", value: "hail", descriptionText: "Switch button was pressed", displayed: false)
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.securityv1.SecurityMessageEncapsulation cmd) {
+	def encapsulatedCommand = cmd.encapsulatedCommand([0x20: 1, 0x25: 1])
+	if (encapsulatedCommand) {
+		zwaveEvent(encapsulatedCommand)
+	}
+}
+
+def zwaveEvent(physicalgraph.zwave.Command cmd) {
+	log.debug "Unhandled: $cmd"
+	null
+}
+
+def on() {
+	commands([
+		zwave.basicV1.basicSet(value: 0xFF),
+		zwave.basicV1.basicGet()
+	])
+}
+
+def off() {
+	commands([
+		zwave.basicV1.basicSet(value: 0x00),
+		zwave.basicV1.basicGet()
+	])
+}
+
+def ping() {
+	refresh()
+}
+
+def poll() {
+	refresh()
+}
+
+def refresh() {
+	command(zwave.basicV1.basicGet())
+}
+
+private command(physicalgraph.zwave.Command cmd) {
+	if ((zwaveInfo.zw == null && state.sec != 0) || zwaveInfo?.zw?.contains("s")) {
+		zwave.securityV1.securityMessageEncapsulation().encapsulate(cmd).format()
+	} else {
+		cmd.format()
+	}
+}
+
+private commands(commands, delay = 200) {
+	delayBetween(commands.collect { command(it) }, delay)
+}
+
+def setLevel(value) {
+}
+
+def setLevel(value, duration) {
+}
